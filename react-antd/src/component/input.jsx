e1bdd0a0d9f3fa9ea2eaba0df894240aefd2d19c
sonicwater <sonicwater@126.com>
2018-06-08 23:00:13 +0800

    props

---
+++ react-antd/src/component/input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Input extends React.Component{
+	constructor(){
+		super();
+	}
+	render(){
+		return(
+			<div>{this.props.abc}</div>
+		)
+	}
+}
+
--- react-antd/src/js/index.jsx
+++ react-antd/src/js/index.jsx
@@ -1,17 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Input from '../component/input.jsx';
 
 class Index extends React.Component{
 	constructor(){
 		super();
 		this.state = {
-			list:[1,2,3,4]
+			list:[],
+			value:""
 		}
 	}
+	add(){
+		let arr = this.state.list;
+		arr.push(this.state.value);
+		this.setState({
+			value:"",
+			list:arr
+		})
+	}
+	change(e){
+		this.setState({
+			value:e.target.value
+		});
+	}
+	delete(index){
+		let arr = this.state.list;
+		arr.splice(index,1);
+		this.setState({
+			list:arr
+		});
+	}
     render(){
+    	let data = this.state.list;
 		return(
 			<div>
-				{this.state.list}
+				<ul>
+					{data.map((item,index) => {
+						return <li key={index} onClick={this.delete.bind(this,index)}>{item}</li>
+					})}
+				</ul>
+				<input type="text" value={this.state.value} onChange={this.change.bind(this)} />
+				<button onClick={this.add.bind(this)}>add</button>
+				<Input abc={this.state.list} />
 		  	</div>
 		);
 	}
